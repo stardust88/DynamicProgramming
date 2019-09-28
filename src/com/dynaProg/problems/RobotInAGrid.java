460820842656093ff3ee4a82f7d31471d0a55af9
deepm <deepm@LAPTOP-UAPLMB3V>
2019-09-28 18:03:38 +0530

    addinh hashcode equals

--- src/com/dynaProg/problems/RobotInAGrid.java
+++ src/com/dynaProg/problems/RobotInAGrid.java
@@ -96,6 +96,31 @@ class Point {
 	private int col;
 
 	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + col;
+		result = prime * result + row;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Point other = (Point) obj;
+		if (col != other.col)
+			return false;
+		if (row != other.row)
+			return false;
+		return true;
+	}
+
+	@Override
 	public String toString() {
 		return "[" + row + "," + col + "]";
 	}
